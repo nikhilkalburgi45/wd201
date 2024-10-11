f9387f98694ce5ac48f6c30626d32fb382b03daf
nikhilkalburgi45 <nikhilkalburgi21@gmail.com>
2024-10-11 21:38:17 +0530

    first commit

--- http-server/index.js
+++ http-server/index.js
@@ -1,24 +1,30 @@
-const express = require("express");
-const path = require("path");
-const minimist = require("minimist");
+const express = require('express');
+const minimist = require('minimist');
+const path = require('path');
 
 const app = express();
 const args = minimist(process.argv.slice(2));
-const port = args.port || 3000;
+const PORT = args.port || 3000; // Default to port 3000 if no port is provided
 
-app.get("/registration", (req, res) => {
-  res.sendFile(path.join(__dirname, "registration.html"));
+// Serve static files from the http-server directory
+app.use(express.static(path.join(__dirname)));
+
+// Route to serve the home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'home.html'));
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "project.html"));
+// Route to serve the projects page
+app.get('/project', (req, res) => {
+    res.sendFile(path.join(__dirname, 'project.html'));
 });
 
-app.get("/home", (req, res) => {
-  res.sendFile(path.join(__dirname, "home.html"));
+// Route to serve the registration page
+app.get('/registration', (req, res) => {
+    res.sendFile(path.join(__dirname, 'registration.html'));
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
