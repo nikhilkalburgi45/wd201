28daa5b38563472adffda63979068b686e119265
nikhilkalburgi45 <nikhilkalburgi21@gmail.com>
2024-10-14 17:04:48 +0530

    first commit

--- http-server/index.js
+++ http-server/index.js
@@ -1,32 +1,31 @@
+// index.js
 const express = require('express');
 const minimist = require('minimist');
 const path = require('path');
 
 const app = express();
-
-// Use minimist to accept port as a command line argument
 const args = minimist(process.argv.slice(2));
-const PORT = args.port || 3000;  // Default to port 3000 if none provided
+const port = args.port || 3000; // Default port 3000
 
-// Serve static files from the root of http-server folder
+// Serve static files
 app.use(express.static(path.join(__dirname)));
 
-// Route to serve the home page
+// Home route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'home.html'));
 });
 
-// Route to serve the project page
+// Project route
 app.get('/project', (req, res) => {
     res.sendFile(path.join(__dirname, 'project.html'));
 });
 
-// Route to serve the registration page
+// Registration route
 app.get('/registration', (req, res) => {
     res.sendFile(path.join(__dirname, 'registration.html'));
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
